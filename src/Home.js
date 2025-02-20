25eae337fdaf337dc373a9c1237d96aa21a40c63
Christopher Flores <Christopher321flores@gmail.com>
2025-02-19 22:15:19 -0500

    Fixing bug within the sign in page

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,23 @@
+import React from 'react';
 import './StyleSheet/Home.css'
 import './StyleSheet/General.css'
 import cherryBloosm from './img/cherryBlossom.jpg'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Signin from './Frontend/Signin';
+// import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+//import Signin from './Frontend/Signin';
 
 function Home(){
     return (
         <body>
-            <Router>
-                <nav class = "nav-bar">
-                    <p class = "title">暢気日本語</p>
-                    <div class = "buttons">
-                        {/* Trying to fix the this in order to click an move on to the signin page */}
-                        | <Link to = "/">Sign in</Link>
-                        <button class="signup">Sign up</button>
-                    </div>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<Signin />} />
-                </Routes>
-            </Router>
+            <nav class = "nav-bar">
+                <p class = "title">暢気日本語</p>
+                <div class = "buttons">
+                    {/* Trying to fix the this in order to click an move on to the signin page */}
+                    <Link to = "/signin">Sign in</Link>
+                    <button class="signup">Sign up</button>
+                </div>
+            </nav>
+                
             <div class = "content">
                 <h1>暢気日本語</h1>
                 <div class="middle-image">
@@ -30,7 +28,7 @@ function Home(){
                 <p class="p1">Welcome to Easy Japanese!</p>
                 <p class="p2">Learning Japanese easier an better</p>
             </div>
-
+            
         </body>
     );
 }
