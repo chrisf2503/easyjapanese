4cf145906dfec98a0a1600adf3028509912e0faf
Christopher Flores <Christopher321flores@gmail.com>
2025-02-22 15:57:12 -0500

    printing the name an the password within the consol to make sure what ever the user type is accurate

--- src/Frontend/Signin.js
+++ src/Frontend/Signin.js
@@ -1,18 +1,45 @@
 import '../StyleSheet/General.css'
 import '../StyleSheet/Signin.css'
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+// function Form(email, password){
+//     if(email.length > 0 && password.length>0){
+//         return true;
+//     }
+// }
 
 function Signin(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    //here we would want to check if the email is valid and if the password
+    //is a strong password
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // alert(`The email you entered was: ${email}`);
+        // alert(`The password you entered was: ${password}`);
+        console.log(email);
+        console.log(password);
+    }
+    
     return(
         <div className = "body">
             <div className = "signIn">
                 <h1>Sign In</h1>
                 <div className="form">
-                    <form>
-                        <label for="email">Email:</label><br/>
-                        <input type="text" id = "email" name="name"/>
+                    <form onSubmit={handleSubmit}>
+                        <label for="email">
+                            Email:<br/>
+                            <input type="text" id = "email" value = {email} onChange={(e => setEmail(e.target.value))}/>    
+                        </label>
                         <br/><br/>
-                        <label for="password">Password:</label><br/>
-                        <input type="text" id = "password" name = "password"/>
+                        <label for="password">
+                            Password:<br/>
+                            <input type="text" id = "password" value = {password} onChange={((e => setPassword(e.target.value)))}/>
+                        </label>
+                        <br/>
+                        <input className='submit' type='submit'/>
                     </form>
                 </div>
             </div>
@@ -20,5 +47,7 @@ function Signin(){
     );
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Signin />);
 
 export default Signin;
